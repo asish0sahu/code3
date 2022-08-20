906ed70cc200c2862aba125d45e8056542c604d7
root <Enter you email ID here>
2022-08-20 14:41:15 +0000

    city

---
+++ names/index.js
@@ -0,0 +1,7 @@
+const peopleName = require("../country/state/city/index");
+const getFirstName = require("../utilities/utils/index");
+
+const getPeopleInCity = (peopleName) => {
+  return getFirstName(peopleName);
+};
+module.exports = getPeopleInCity;
